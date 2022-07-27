f12e6e051284b35ccb0145a7e8c40aee8926917a
Nitika <nitika.vashisht@tietoevry.com>
2022-07-27 10:44:38 +0530

    Navgar Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,12 +24,12 @@ export class AppComponent {
     // this is used to call property
     this.eventBindind = "Something is created";
   }
-  
-  onCart(){
+
+  onCart() {
     this.cartInfo = "Added tour cart";
   }
 
-  cartInfo:string = "";
+  cartInfo: string = "";
   custommethod() {
     return ("This is custom method" + this.tittle);
   }
@@ -43,36 +43,54 @@ export class AppComponent {
 
   eventBindind = "Nothing is created"
 
-  onClick(event: any){
+  onClick(event: any) {
     console.log(event.target.value);
   }
-  
-  onPhone(event: any){
-    this.phoneclicked = (event.target.value  +  " is clicked");
+
+  onPhone(event: any) {
+    this.phoneclicked = (event.target.value + " is clicked");
   }
   // onLaptop(){
   //   this.plaptopclicked = "laptop is clicked";
   // }
-  phoneclicked:string = "";
-  plaptopclicked:string = "";
+  phoneclicked: string = "";
+  plaptopclicked: string = "";
 
-  onCart1(inputinfo:any){
-    console.log(inputinfo.value);
-    console.log(inputinfo.value)
-     }
-
-
-
-    //  onCart1(my:any){
-    //   console.log(my.value);
-    //   console.log(my.name)
-    //    }
-
-    dataBinding:string= "Two Way Binding";
+  onCart1(inputinfo1: any) {
+    console.log(inputinfo1.value);
+    console.log(inputinfo1.name)
+  }
 
 
-    option:boolean = false;
-    onOption(){
-      this.option= true;
-    }
+
+  //  onCart1(my:any){
+  //   console.log(my.value);
+  //   console.log(my.name)
+  //    }
+
+  dataBinding: string = "Two Way Binding";
+
+
+  option: boolean = false;
+  onOption() {
+    this.option = true;
+  }
+  product = [
+    { proimg: 'https://i.postimg.cc/WpBc133P/winter-blue-pink-gradient-background-vector-53876-117276.webp', name: 'laptop', price: '1000', id: '1', },
+    { proimg: 'https://i.postimg.cc/WpBc133P/winter-blue-pink-gradient-background-vector-53876-117276.webp', name: 'laptop1', price: '2000', id: '2', },
+    { proimg: 'https://i.postimg.cc/4NysmMvz/8b56ffb305d960f5-800x800ar.png', name: 'laptop2', price: '3000', id: '3', },
+    { proimg: 'https://i.postimg.cc/WpBc133P/winter-blue-pink-gradient-background-vector-53876-117276.webp', name: 'laptop3', price: '4000', id: '4', }
+  ]
+
+
+
+
+
+  userArray= [];
+  // onCreate(inputValue1:any){
+  //   //  console.log(inputValue1.value) ;
+  //   this.userArray.push({
+  //   name.inputValue1.value
+  //   })
+  //     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './Mycomponent/todo-list/todo-list.component';
@@ -8,18 +10,30 @@ import { Card1Component } from './Mycomponent/card1/card1.component';
 import { Card2Component } from './Mycomponent/card2/card2.component';
 import { MessageServiceService } from './message-service.service';
 import { SampleserviceService } from './sampleservice.service';
+import { HomeComponent } from './Mycomponent/home/home.component';
+import { AboutComponent } from './Mycomponent/about/about.component';
+
+
+const appRoutes: Routes=[
+  {path:'', component: HomeComponent},
+  {path:'about', component: AboutComponent}
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent,
     Card1Component,
-    Card2Component
+    Card2Component,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [MessageServiceService,SampleserviceService],
   bootstrap: [AppComponent]
